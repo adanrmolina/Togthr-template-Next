9fb1b02ab991a21dae5bd5a45594a2b50186d922
adanrmolina <107446738+adanrmolina@users.noreply.github.com>
2025-09-22 12:10:08 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',      // genera sitio estático al hacer "next build"
-  trailingSlash: true,   // crea /ruta/index.html (recomendado en hosting estático)
+  trailingSlash: true, // opcional, útil en hosting estático
 };
 module.exports = nextConfig;
