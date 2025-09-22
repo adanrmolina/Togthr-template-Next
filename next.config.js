cf36d0c8d9b38c2abb0e47fc40d8ec86772653bc
adanrmolina <107446738+adanrmolina@users.noreply.github.com>
2025-09-22 11:56:30 -0600

    Commit from TeleportHQ

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
-// next.config.js
-const isStatic = process.env.NEXT_EXPORT === 'true';
-
 module.exports = {
-  images: { unoptimized: true },
-  ...(isStatic ? {} : {
-    i18n: {
-      locales: ['en', 'es'],
-      defaultLocale: 'es',
-      localeDetection: false,
-    }
-  })
-};
+  i18n: {
+    locales: ['en', 'es'],
+    defaultLocale: "es",
+    localeDetection: false,
+  }
+}
