63b718efee04163a1014dfde0bb393629d7805fc
adanrmolina <107446738+adanrmolina@users.noreply.github.com>
2025-09-22 11:40:19 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
+// next.config.js
+const isStatic = process.env.NEXT_EXPORT === 'true'; // lo usaremos en build y export
+
 module.exports = {
-  i18n: {
-    locales: ['en', 'es'],
-    defaultLocale: "es",
-    localeDetection: false,
-  }
-}
+  // Para que next/image funcione en export estático (Next 12)
+  images: { unoptimized: true },
+
+  // Habilita i18n SOLO cuando NO estamos haciendo build/export estático
+  ...(isStatic ? {} : {
+    i18n: {
+      locales: ['en', 'es'],
+      defaultLocale: 'es',
+      localeDetection: false,
+    }
+  })
+};
