a3fc60446ae726a03757e40436879e26ab812e81
adanrmolina <107446738+adanrmolina@users.noreply.github.com>
2025-09-22 11:31:29 -0600

    Commit from TeleportHQ

--- next.config.js
+++ next.config.js
@@ -1,16 +1,7 @@
-// next.config.js
-const isStatic = process.env.NEXT_EXPORT === 'true'; // usaremos esto en build y export
-
 module.exports = {
-  // Para que next/image funcione en export estático (Next 12)
-  images: { unoptimized: true },
-
-  // Si NO estamos haciendo build/export estático, entonces habilita i18n
-  ...(isStatic ? {} : {
-    i18n: {
-      locales: ['en', 'es'],
-      defaultLocale: 'es',
-      localeDetection: false,
-    }
-  })
-};
+  i18n: {
+    locales: ['en', 'es'],
+    defaultLocale: "es",
+    localeDetection: false,
+  }
+}
