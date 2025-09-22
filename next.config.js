e96cd18c638fac895d58baab07e23e65dca064f9
adanrmolina <107446738+adanrmolina@users.noreply.github.com>
2025-09-22 11:19:24 -0600

    Commit from TeleportHQ

--- next.config.js
+++ next.config.js
@@ -1,15 +1,7 @@
-const isExport = process.env.NEXT_EXPORT === 'true';
-
-const nextConfig = {
-  output: 'export',          // Next 13+
-  images: { unoptimized: true }, // requerido para export estático si usas next/image
-  ...(isExport ? {} : {
-    // Solo habilita i18n cuando NO estás haciendo export estático
-    i18n: {
-      locales: ['es', 'en'],
-      defaultLocale: 'es',
-    }
-  })
-};
-
-module.exports = nextConfig;
+module.exports = {
+  i18n: {
+    locales: ['en', 'es'],
+    defaultLocale: "es",
+    localeDetection: false,
+  }
+}
